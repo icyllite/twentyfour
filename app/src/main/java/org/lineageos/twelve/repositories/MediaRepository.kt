c19abb1b9fa45455ac86c5b10b766461356bb063
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-22 16:43:40 +0100

    Twelve: Don't state mediaStoreProviders
    Change-Id: I9edfa526bd89b501ed36e6a0c500bab28a067115

--- app/src/main/java/org/lineageos/twelve/repositories/MediaRepository.kt
+++ app/src/main/java/org/lineageos/twelve/repositories/MediaRepository.kt
@@ -98,12 +98,6 @@ class MediaRepository(
                 .sortedBy { it.isPrimary.not() }
         }
         .distinctUntilChanged()
-        .flowOn(Dispatchers.IO)
-        .stateIn(
-            scope,
-            SharingStarted.WhileSubscribed(),
-            listOf()
-        )
 
     private val mediaStoreProviders = combine(
         sharedPreferences.preferenceFlow(
@@ -142,12 +136,6 @@ class MediaRepository(
             }
         }
     }
-        .flowOn(Dispatchers.IO)
-        .stateIn(
-            scope,
-            SharingStarted.WhileSubscribed(),
-            listOf()
-        )
 
     /**
      * HTTP cache
